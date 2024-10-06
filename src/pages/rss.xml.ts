5a052bd4596f2bc279ee4cce5407e4aad415425d
Israel Salinas <israelsalinashn@gmail.com>
2024-10-06 15:48:02 -0600

    Image y contenido en el news feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss'
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
 
@@ -8,7 +12,9 @@ export const GET: APIRoute = async ({ params, request, site }) => {
 
     return rss({
         // stylesheet: '/styles/rss.xsl',
-
+        xmlns: {
+            media: 'http://search.yahoo.com/mrss/',
+        },
         // `<title>` field in output xml
         title: 'Israel Salinas’s Blog',
         // `<description>` field in output xml
@@ -18,13 +24,26 @@ export const GET: APIRoute = async ({ params, request, site }) => {
         site: site ?? '',
         // Array of `<item>`s in output xml
         // See "Generating items" section for examples using content collections and glob imports
-        items: blogPost.map(({ data, slug }) => ({
+        items: blogPost.map(({ data, slug, body }) => ({
             title: data.title,
             pubDate: data.date,
             description: data.description,
             link: `/post/${slug}`,
+
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+            }),
+
+            customData: `<media:content
+                  type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                  width="${data.image.width}"
+                  height="${data.image.height}"
+                  medium="image"
+                  url="${site + data.image.src}" />
+              `,
+
         })),
-        // (optional) inject custom xml
+        //  (optional) inject custom xml
         customData: `<language>es-es</language>`,
     });
 }
