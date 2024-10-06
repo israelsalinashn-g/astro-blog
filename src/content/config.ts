6f3a1fee30b5a2420fb7190f94d4d2e8762ecc21
Israel Salinas <israelsalinashn@gmail.com>
2024-10-05 22:52:32 -0600

    Fin sección 6

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
     type: 'content',
@@ -11,12 +11,31 @@ const blogCollection = defineCollection({
         }),
 
         //relación
-        author: z.string(),
+        // author: z.string(),
+        author: reference('author'),
+
         //relación
         tags: z.array(z.string()),
+
+        // Boolean
+        isDraft: z.boolean().default(false),
     }),
 });
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
+    }),
+})
+
 export const collections = {
     blog: blogCollection,
+    author: authorCollection,
 }
